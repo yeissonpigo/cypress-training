b5ae586d6935ea679e3166b60c397875d01d9b08
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-08-17 16:28:27 -0500

    Added steps until clicking on add to cart button

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -3,7 +3,12 @@ describe("Buy a t-shirt", () => {
     // Goes to page
     cy.visit("http://automationpractice.com/index.php");
 
-    // Finds t-shirt button
-    cy.get("#block_top_menu>ul>li").eq(2).children('a').click();
+    // Finds t-shirt button and clicks it
+    cy.get("#block_top_menu > ul > li").eq(2).children('a').click()
+
+    // Hovers over product to show add to cart button
+    cy.get('.product_list > li:nth-child(1)').trigger('mouseover')
+    // Finds add to cart button and clicks it
+    cy.get('.product-container > .right-block > .button-container').contains('Add to cart').click()
   });
 });
