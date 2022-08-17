20c4c62bf949ba20dc5b62d10e2849333c5af503
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-08-17 17:48:38 -0500

    Fixed eslint conflicts

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // modify config values examples
       // config.defaultCommandTimeout = 10000
+      config.defaultCommandTimeout = 20000
+      config.responseTimeout = 20000
 
       // IMPORTANT return the updated config object
       return config;
--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -4,39 +4,38 @@ describe("Buy a t-shirt", () => {
     cy.visit("http://automationpractice.com/index.php");
 
     // Finds t-shirt button and clicks it
-    cy.get("#block_top_menu > ul > li").eq(2).children('a').click()
+    cy.get("#block_top_menu > ul > li").eq(2).children("a").click();
 
     // Hovers over product to show add to cart button
-    cy.get('.product_list > li:nth-child(1)').trigger('mouseover')
+    cy.get(".product_list > li:nth-child(1)").trigger("mouseover");
     // Finds add to cart button and clicks it
-    cy.get('.product-container > .right-block > .button-container').contains('Add to cart').click().then((popUp) => {
-        // Clicks on Proceed to checkout button  
-        cy.wait(5000)
-        cy.get('#layer_cart > .clearfix > div:nth-child(2) > .button-container > a').click()
-    })
+    cy.get(".product-container > .right-block > .button-container").contains("Add to cart").click().then((popUp) => {
+      // Clicks on Proceed to checkout button
+      cy.get("#layer_cart > .clearfix > div:nth-child(2) > .button-container > a").click();
+    });
 
     // Clicks on Proceed to checkout button (different page than before)
-    cy.get('#center_column > .cart_navigation > a:nth-child(1)').click()
+    cy.get("#center_column > .cart_navigation > a:nth-child(1)").click();
 
     // Inserts login information and clicks on login
-    cy.get("#email").type("aperdomobo@gmail.com")
-    cy.get("#passwd").type("WorkshopProtractor")
-    cy.get('#SubmitLogin').click()
+    cy.get("#email").type("aperdomobo@gmail.com");
+    cy.get("#passwd").type("WorkshopProtractor");
+    cy.get("#SubmitLogin").click();
 
     // Clicks on Proceed to checkout button at address stage
-    cy.get('button[name="processAddress"]').click()
+    cy.get("button[name=\"processAddress\"]").click();
 
     // Checks checkbox to agree on terms
-    cy.get('#cgv').click()
+    cy.get("#cgv").click();
     // Clicks on Proceed to checkout at shipping stage
-    cy.get('#form > p > button[name="processCarrier"]').click()
+    cy.get("#form > p > button[name=\"processCarrier\"]").click();
 
     // Selects payment method
-    cy.get('a.bankwire').click()
+    cy.get("a.bankwire").click();
     // Confirms order
-    cy.get('#center_column > form > p > button[type="submit"]').click()
+    cy.get("#center_column > form > p > button[type=\"submit\"]").click();
 
     // Checks order was completed
-    cy.get('.cheque-indent > strong').should('have.text', 'Your order on My Store is complete.')
+    cy.get(".cheque-indent > strong").should("have.text", "Your order on My Store is complete.");
   });
 });
