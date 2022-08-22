b03e9a4e1b008e4ef3653d4272cec46d5bb41492
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-08-22 15:11:21 -0500

    updated products-list.page.ts and buy-shirt.cy.ts

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -1,9 +1,9 @@
 import * as pom from "../page/index";
 
 const menuContentPage = new pom.MenuContentPage();
-const productsListPage = new pom.ProductsListPage(1);
+const productsListPage = new pom.ProductsListPage();
 const shopingCartPage = new pom.ShopingCartPage();
-const loginPage = new pom.LoginPage("aperdomobo@gmail.com", "WorkshopProtractor");
+const loginPage = new pom.LoginPage();
 const addressStepPage = new pom.AddressStepPage();
 const shippingStepPage = new pom.ShippingStepPage();
 const paymentStepPage = new pom.PaymentStepPage();
@@ -11,39 +11,17 @@ const confirmationPage = new pom.ConfirmationPage();
 
 describe("Buy a t-shirt", () => {
   it("finds t-shirt button and clicks on it", ()=>{
-    // Goes to page
+    
     menuContentPage.visitMenuContentPage();
-
-    // Finds t-shirt button and clicks it
     menuContentPage.goToTShirtMenu();
-
-    // Hovers over product to show add to cart button
-    productsListPage.hoverOnProduct();
-
-    // Finds add to cart button and clicks it
-    productsListPage.addToCart();
-    // Clicks on Proceed to checkout button (different page than before)
+    productsListPage.addToCart(1);
     shopingCartPage.clickProceed();
-
-    // Inserts login information and clicks on login
-    loginPage.enterEmail();
-    loginPage.enterPasswd();
-    loginPage.loginButtonFun();
-
-    // Clicks on Proceed to checkout button at address stage
+    loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
     addressStepPage.checkout();
-
-    // Checks checkbox to agree on terms
     shippingStepPage.clickCheckbox();
-    // Clicks on Proceed to checkout at shipping stage
     shippingStepPage.checkoutFun();
-
-    // Selects payment method
     paymentStepPage.selectPaymentMethod();
-    // Confirms order
     paymentStepPage.confirmOrderFun();
-
-    // Checks order was completed
     confirmationPage.checksConfirmation();
   });
 });
--- cypress/page/products-list.page.ts
+++ cypress/page/products-list.page.ts
@@ -1,21 +1,16 @@
 class ProductsListPage {
-    private productNumber: number
     private addToCartButton: string
     private addToCartButtonContainer: string
     private addToCartButtonText: string
 
-    constructor(productNumber: number) {
-        this.productNumber = productNumber
+    constructor() {
         this.addToCartButton = "#layer_cart > .clearfix > div:nth-child(2) > .button-container > a"
         this.addToCartButtonContainer = ".product-container > .right-block > .button-container"
         this.addToCartButtonText = "Add to cart"
     }
 
-    public hoverOnProduct() {
-        cy.get(`.product_list > li:nth-child(${this.productNumber})`).trigger("mouseover")
-    }
-
-    public addToCart() {
+    public addToCart(productNumber: number) {
+        cy.get(`.product_list > li:nth-child(${productNumber})`).trigger("mouseover")
         cy.get(this.addToCartButtonContainer).contains(this.addToCartButtonText).click().then((popUp) => {
             // Clicks on Proceed to checkout button
             cy.get(this.addToCartButton).click()
