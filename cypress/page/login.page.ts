c31b1517aee727d7302d1184d769fa006a2f48b8
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-08-22 15:06:34 -0500

    Deleted global variables email and passwd since now they are local variables at login function

--- cypress/page/login.page.ts
+++ cypress/page/login.page.ts
@@ -1,15 +1,11 @@
 class LoginPage {
     
-    private email: string
     private emailInput: string
-    private passwd: string
     private passwdInput: string
     private loginButton: string
 
-    constructor(email: string, passwd: string) {
-        this.email = email
+    constructor() {
         this.emailInput = "#email"
-        this.passwd = passwd
         this.passwdInput = "#passwd"
         this.loginButton = "#SubmitLogin"
     }
