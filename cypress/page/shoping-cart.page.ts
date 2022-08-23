d751eb74bce575c2e119b7d20a16d2d17263cc06
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-08-23 09:38:48 -0500

    Changed Shoping-cart page selectors

--- cypress/page/shoping-cart.page.ts
+++ cypress/page/shoping-cart.page.ts
@@ -3,7 +3,7 @@ class ShopingCartPage {
     private checkoutButton : string
     
     constructor() {
-        this.checkoutButton = "#center_column > .cart_navigation > a:nth-child(1)"
+        this.checkoutButton = "#center_column > .cart_navigation.clearfix > a"
     }
 
     public clickProceed() {
