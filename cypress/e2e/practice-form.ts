4d1d493f880d08392eb7378017e3d002c643cd0b
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-09-12 17:34:58 -0500

    idk

---
+++ cypress/e2e/practice-form.ts
@@ -0,0 +1,28 @@
+import {PracticeFormPage} from "../page/index";
+
+describe("User goes to website, fills the form and submits it", () => {
+  let practiceFormPage: PracticeFormPage;
+
+  before(() => {
+    practiceFormPage = new PracticeFormPage();
+  });
+  it("checks the right results", () => {
+    // Arrange
+    const personalInformation = {
+      name: "Holmes",
+      lastName: "Salazar",
+      email: "test@email.com",
+      gender: "Male",
+      dateOfBirth: "27 Jul 2016",
+      mobileNumber: "3656589156",
+      hobbies: ["Music", "Reading"],
+      currentAddress: "Av siempreViva # 123",
+    };
+    practiceFormPage.visitFormContentPage();
+
+    // Act
+    practiceFormPage.fillForm(personalInformation);
+
+    // Assert
+  });
+});
---
+++ cypress/page/practice-form.page.ts
@@ -0,0 +1,72 @@
+class PracticeFormPage {
+
+    private transformHobby = {
+        "Sports": 1,
+        "Reading": 2,
+        "Music": 3
+    }
+
+    private getSelectorInputById(inputId: string) {
+        return `#${inputId}`
+    }
+
+    private getSelectorGenderByText(gender: string) {
+        return `[value="${gender}"]`
+    }
+
+    private getSelectorHobbieByPosition(position: string) {
+        return `[value="${position}"]`
+    }
+
+    private getSelectorSubmitButton = "#submit"
+
+    private formContentPage: string
+
+    constructor() {
+        this.formContentPage = "https://demoqa.com/automation-practice-form"
+    }
+
+    public click(element: string) {
+        cy.get(element).click()
+    }
+
+    public visitFormContentPage() {
+        cy.visit(this.formContentPage)
+    }
+
+    public insertIntoInput(selector: string, text: string) {
+        cy.get(this.getSelectorInputById(selector)).type(text)
+    }
+
+    public selectGender(gender: string) {
+        cy.get(this.getSelectorGenderByText(gender)).click()
+    }
+
+    public selectHobby(hobby: string) {
+        cy.get(this.getSelectorHobbieByPosition(hobby)).click()
+    }
+
+    public fillForm(information: {
+        name: string,
+        lastName: string,
+        email: string,
+        gender: string,
+        dateOfBirth: string,
+        mobileNumber: string,
+        hobbies: Array<string>,
+        currentAddress: string
+    }) {
+        this.insertIntoInput("firstName", information.name)
+        this.insertIntoInput("lastName", information.lastName)
+        this.insertIntoInput("userEmail", information.email)
+        this.selectGender(information.gender)
+        this.insertIntoInput("userNumber", information.mobileNumber)
+        this.insertIntoInput("dateOfBirthInput", information.dateOfBirth)
+        information.hobbies.forEach((hobbie) => {
+            this.selectHobby(`this.transformHobby.${hobbie}`)
+        })
+        this.insertIntoInput("currentAddress", information.currentAddress)
+        this.click(this.getSelectorSubmitButton)
+    }
+}
+export { PracticeFormPage }
