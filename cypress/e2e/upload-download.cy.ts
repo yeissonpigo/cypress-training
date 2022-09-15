b83a8811e50718031d5d9665a1684b07e7970398
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-09-15 12:45:06 -0500

    test

--- cypress/e2e/upload-download.cy.ts
+++ cypress/e2e/upload-download.cy.ts
@@ -1,21 +1,21 @@
-import {UploadPage} from "../page";
+import { UploadPage } from "../page";
 
 describe("Uploads a file, and checks that the name is the right one", () => {
-  let uploadPage: UploadPage;
+    let uploadPage: UploadPage;
 
-  before(() => {
-    uploadPage = new UploadPage;
-  });
+    before(() => {
+        uploadPage = new UploadPage;
+    });
 
-  it("Uploads a file", () => {
-    // Arrange
-    const fileToUpload = "example.json";
+    it("Uploads a file", () => {
+        // Arrange
+        const fileToUpload = "example.json";
 
-    // Act
-    uploadPage.visitPage();
-    uploadPage.uploadFile(fileToUpload);
+        // Act
+        uploadPage.visitPage();
+        uploadPage.uploadFile(fileToUpload);
 
-    // Assert
-    uploadPage.checkUploadedFileName(fileToUpload);
-  });
+        // Assert
+        uploadPage.checkUploadedFileName(fileToUpload);
+    });
 });
