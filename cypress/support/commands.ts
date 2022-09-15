471dee07bf02989bf7be64189d2cfae69947df0c
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-09-15 10:40:59 -0500

    Set up upload files library

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,6 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 // Ignoring uncaught exceptions since errors from external apps should not stop de workshop
+import 'cypress-file-upload';
 Cypress.on("uncaught:exception", (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
