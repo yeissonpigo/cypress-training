52597c36cd6d188177f8164cf7a1dd8cd5b20492
Yeisson Pinilla Gonzalez <yeisson.pinillag@perficient.com>
2022-09-16 09:28:31 -0500

    added iframe library

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -25,6 +25,7 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 // Ignoring uncaught exceptions since errors from external apps should not stop de workshop
 import 'cypress-file-upload';
+import 'cypress-iframe';
 Cypress.on("uncaught:exception", (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
